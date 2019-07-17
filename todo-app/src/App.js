9e9327871c5a649e26ec420490de57baf3e8dd84
thainguyen1912 <32010592+thainguyen1912@users.noreply.github.com>
2019-07-17 12:00:20 +0700

    finish-todoapp

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,30 +1,109 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import TrafficLight from './Components/TrafficLight';
+import TodoItem from './Components/TodoItem';
+import checkAll from './icon/success-active.svg';
 
 class App extends Component {
-  // constructor(props){
-  //   super(props);
-  //   this.TodoItems=[
-  //     {title : 'Mua Bim Bim', isComplete:true},
-  //     {title : 'Đi Đá Bóng', isComplete:true},
-  //     {title : 'Ngủ'}
-  //   ];
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: '',
+      status: true,
+      TodoItems: [
+        { title: 'Mua Bim Bim', isComplete: true },
+        { title: 'Đi Đá Bóng', isComplete: true },
+        { title: 'Ngủ', isComplete: false }
+      ]
+    }
+    this.onKeyUp = this.onKeyUp.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.onClickAll = this.onClickAll.bind(this);
+  }
 
-  render(){
-  return (
-    <div className="App">
-       {/* {
-          this.TodoItems.length > 0 &&
-        this.TodoItems.map((item, index) => <TodoItem item={item} key={index}/>)
-        
+  onClick(item) {
+    return () => {
+      const { TodoItems } = this.state;
+      const isComplete = item.isComplete;
+      const index = this.state.TodoItems.indexOf(item);
+      this.setState({
+        TodoItems: [
+          ...TodoItems.slice(0, index),
+          {
+            ...item, isComplete: !isComplete
+          },
+          ...TodoItems.slice(index + 1)
+        ]
+      })
+    }
+  }
+  onKeyUp(event) {
+    let text = event.target.value;
+    if (event.keyCode === 13) {
+
+      if (!(text.trim())) {
+        return;
       }
-      {this.TodoItems.length===0 && 'Nothing here'}  */}
-      <TrafficLight />
 
-    </div>
-  );
+      this.setState({
+        newItem: '',
+        TodoItems: [
+          {
+            title: text,
+            isComplete: false
+          },
+          ...this.state.TodoItems
+        ]
+      })
+    } else {
+      this.setState({
+        newItem: text
+      })
+    }
+  }
+  onChange(event) {
+    this.setState({
+      newItem: event.target.value
+    })
+  }
+  onClickAll() {
+    const status = this.state.TodoItems.filter(i => i.isComplete === false);
+    const { TodoItems: arr } = this.state;
+    console.log(status);
+    if (status.length > 0) {
+      arr.map((elements) => (elements.isComplete = true));
+      this.setState({
+        TodoItems: arr
+      })
+    }
+    else {
+      arr.map((elements) => (elements.isComplete = false));
+      this.setState({
+        TodoItems: arr
+      })
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="Header">
+          <img src={checkAll} onClick={this.onClickAll} />
+          <input type="text"
+            onKeyUp={this.onKeyUp}
+            value={this.state.newItem}
+            onChange={this.onChange}
+          />
+        </div>
+        {
+          this.state.TodoItems.length > 0 &&
+          this.state.TodoItems.map((item, index) => <TodoItem
+            item={item}
+            key={index}
+            onClick={this.onClick(item)} />)
+
+        }
+        {this.state.TodoItems.length === 0 && 'Nothing here'}
+      </div>
+    );
   }
 }
 
--- todo-app/src/Components/TodoItem.js
+++ todo-app/src/Components/TodoItem.js
@@ -1,16 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './TodoItem.css';
-class TodoItem extends Component{
-    render(){
-        const {item}=this.props;
-        let className='TodoItem';
-        if(item.isComplete) className+=' TodoItem-Complete';
-        return(
-            <div className={className}>
+import check from '../icon/success.svg';
+import check_active from '../icon/success-active.svg';
+class TodoItem extends Component {
+    onClick() {
+        this.props.onClick();
+    }
+    render() {
+        const { item } = this.props;
+        let className = 'TodoItem';
+        let url=check;
+        if(item.isComplete){
+            url=check_active;
+        }
+        if (item.isComplete) className += ' TodoItem-Complete';
+        return (
+            <div onClick={()=>this.onClick()} className={className}>
+                <img src={url} width={32}/>
                 <p>{this.props.item.title}</p>
             </div>
-
-        )
+        );
     }
 }
 export default TodoItem;
